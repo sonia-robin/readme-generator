c39f3179de9e25705806dee4fbf642084bc210a5
unknown <cervinkova.sonik@gmail.com>
2024-01-19 19:46:02 +0000

    inquirer.prompt added

--- index.js
+++ index.js
@@ -8,6 +8,49 @@ const questions = [
 
 ];
 
+const promptUser = () => {
+    return inquirer.prompt([
+      {
+        type: 'input',
+        name: 'project',
+        message: 'What is your project name?',
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Please write a short description of your project',
+      },
+      {
+        type: 'list',
+        name: 'licenses',
+        message: 'What kind of licenses should your project have?',
+        choices: ['MIT', 'Apache', 'GPL']
+      },
+      {
+        type: 'input',
+        name: 'installation',
+        message: 'What command should be run to install dependencies?',
+      },
+      {
+        type: 'input',
+        name: 'test',
+        message: 'What command should be run to run tests?',
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'What does the user need to know about using the repo?',
+      },
+      {
+        type: 'input',
+        name: 'contribution',
+        message: 'What does the user need to know about contributing to the repo?',
+      },
+    ]);
+  };
+
+promptUser();
+
 // function to write README file
 function writeToFile(fileName, data) {
 }
