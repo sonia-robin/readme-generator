7bcfab5c16ddcabfae3688d870a0703bb7474999
unknown <cervinkova.sonik@gmail.com>
2024-01-28 21:12:00 +0000

    update generateMarkdown file path

--- assets/index.js
+++ assets/index.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const path = require('path');
 const inquirer = require("inquirer");
-const generateMarkdown = require("../utils/generateMarkdown");
+const generateMarkdown = require("./generateMarkdown");
 const util = require('util');
 const writeFileAsync = util.promisify(fs.writeFile);
 
